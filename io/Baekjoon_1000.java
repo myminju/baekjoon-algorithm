4c1880fa84fa67aa48c39dbb16456a72f90f77c8
myminju <mjeon7890@gmail.com>
2021-07-03 14:25:15 +0900

    1000: A+B

--- io/Baekjoon_1000.java
+++ io/Baekjoon_1000.java
@@ -1,4 +1,12 @@
 package io;
 
+import java.util.Scanner;
+
 public class Baekjoon_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a+b);
+    }
 }
