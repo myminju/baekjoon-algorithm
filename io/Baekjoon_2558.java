eb0fd32380319644d0fae70ebdf120ec7a97f5ae
myminju <mjeon7890@gmail.com>
2021-07-03 14:26:02 +0900

    2558: A+B

---
+++ io/Baekjoon_2558.java
@@ -0,0 +1,13 @@
+package io;
+
+import java.util.Scanner;
+
+public class Baekjoon_2558 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        System.out.print(a+b);
+    }
+}
